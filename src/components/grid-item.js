602957ae0dc80f827d77ae796aa03327cb14b03f
David Flindall <hello@backtofront.london>
2019-11-07 19:09:26 +0100

    tidy components

--- src/components/grid-item.js
+++ src/components/grid-item.js
@@ -1,9 +1,6 @@
 import styled from "styled-components"
-//import theme from "../theme"
 
 export default styled.div `
- 
-
     margin: 0 0 2rem 0;
     display: inline-block;
     width: 100%;
--- src/components/grid-wrapper.js
+++ src/components/grid-wrapper.js
@@ -1,10 +1,6 @@
 import styled from "styled-components"
-//import theme from "../theme"
 
 export default styled.div `
-  /* display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-gap: 50px;  */
   columns: 2;
   column-gap: 2rem;
 `
