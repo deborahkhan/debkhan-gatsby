1c76ec6bb8a0701a0bf60b060efa7cc955597d52
David Flindall <hello@backtofront.london>
2019-11-08 16:07:06 +0100

    "what i do" content

--- src/pages/index.js
+++ src/pages/index.js
@@ -47,56 +47,101 @@ const IndexPage = ( { data } ) => (
       </SplitItem>
     </SplitWrapper>
 
-
-
-
-
     <SubTitle>
       In the last 3 years I have:      
     </SubTitle>
 
     <Grid>
+
       <GridItem>
         <TextWrapper>
-          <p>Guided the luxury brands Orlebar Brown, Annoushka Jewellery and Lucas Hugh to define a distinctive brand position, direction & story, one leading to acquisition by Chanel</p>
+          <p>Guided the luxury brands Orlebar Brown, Annoushka Jewellery and Lucas Hugh to define a distinctive brand position, direction &amp; story, one leading to acquisition by Chanel</p>
         </TextWrapper>
       </GridItem>
-      <GridItem>
-        <TextWrapper>
-          <p>Steered Mikhail Riches - a design driven architecture practice to win The Stirling Prize - awarded for the first 100% council house project in history</p>
-        </TextWrapper>
-      </GridItem>
+
       <GridItem>
         <TextWrapper>
           <p>Worked with small architecture practices Tate Harmer, Cousins and Cousins, MIkhail Riches, and William Mathhews  to win competitions build spends of over £500 million - worked on the brief, the design of the narrative and the pitch and guided one to be 2019 favourite for The Stirling Prize</p>
         </TextWrapper>
       </GridItem>
-      <GridItem>
-        <TextWrapper>
-          <p>Taken a 3 day week role as VP Culture at Kano (present)</p>
-        </TextWrapper>
-      </GridItem>
-      <GridItem>
-        <TextWrapper>
-          <p>Trained over 500 people to present with impact, sell in ideas, network and influence</p>
-        </TextWrapper>
-      </GridItem>
+
       <GridItem>
         <TextWrapper>
           <p>Helped the managers at King.com, makes of Candy Crush be even more effective &amp; lead conversations about performance</p>
         </TextWrapper>
       </GridItem>
-      <GridItem>
-        <TextWrapper>
-          <p>Written a fantastic book, She’s Back  shortlisted for CMI Management Book of the Year 2019 endorsed  by Arianna Huffington</p>
-        </TextWrapper>
-      </GridItem>      
-      <GridItem>
-        <TextWrapper>
-          <p>Supported the top global creative agency Mother to help their teams be more productive, creative & effective </p>
-        </TextWrapper>
-      </GridItem>    
 
+      <GridItem>
+        <TextWrapper>
+          <p>Supported the top global creative agency Mother to help their teams be more productive, creative &amp; effective </p>
+        </TextWrapper>
+      </GridItem>
+
+      <GridItem>
+        <TextWrapper>
+          <p>Steered Parkinson’s UK to lead with rigour &amp; purpose though a change management programme</p>
+        </TextWrapper>
+      </GridItem>
+
+      <GridItem>
+        <TextWrapper>
+          <p>Designed a comms plan for innovative mixed use building scheme in London including a new church, secondary school, shops &amp; 150 new homes</p>
+        </TextWrapper>
+      </GridItem>
+
+      <GridItem>
+        <TextWrapper>
+          <p>Cycled 5600m from Siena to Rome in 3 days 50 senior women in the property industry and raised over £2500 for Coram Foundation  </p>
+        </TextWrapper>
+      </GridItem>
+
+      <GridItem>
+        <TextWrapper>
+          <p>Steered Mikhail Riches - a design driven architecture practice to win The Stirling Prize - awarded for the first 100% council house project in history</p>
+        </TextWrapper>
+      </GridItem>
+
+      <GridItem>
+        <TextWrapper>
+          <p>Taken a 3 day week role as VP Culture at Kano (present)</p>
+        </TextWrapper>
+      </GridItem>
+
+      <GridItem>
+        <TextWrapper>
+          <p>Trained over 500 people to present with impact, sell in ideas, network and influence</p>
+        </TextWrapper>
+      </GridItem>
+
+      <GridItem>
+        <TextWrapper>
+          <p>Written a fantastic book, She’s Back shortlisted for CMI Management Book of the Year 2019 endorsed  by Arianna Huffington</p>
+        </TextWrapper>
+      </GridItem>
+
+      <GridItem>
+        <TextWrapper>
+          <p>Shaped wow comms by designing effective newsletters, events, films &amp;  copy including for design firms, structural engineers and charotes</p>
+        </TextWrapper>
+      </GridItem>
+
+      <GridItem>
+        <TextWrapper>
+          <p>Designed &amp; delivered effective, practical management learning &amp; development programmes for a data company &amp; a digital ad agency </p>
+        </TextWrapper>
+      </GridItem>
+
+      <GridItem>
+        <TextWrapper>
+          <p>Partnered with The International School of the Year British School Muscat to deliver a growth plan</p>
+        </TextWrapper>
+      </GridItem>
+
+      <GridItem>
+        <TextWrapper>
+          <p>Featured as a writer for GQ (UK) and Strategy+Business (global)</p>
+        </TextWrapper>
+      </GridItem>                                                                              
       
 
 
