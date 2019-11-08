391138da56ead1450a1fc8d45a46c094db0bd016
David Flindall <hello@backtofront.london>
2019-11-08 18:03:48 +0100

    padding for text wrappers small screens

--- src/components/text-wrapper.js
+++ src/components/text-wrapper.js
@@ -2,8 +2,13 @@ import styled from "styled-components"
 import theme from "../theme"
 
 export default styled.div `
+
+  @media (min-width: 576px) {
+    padding: 2.5rem;    
+  }
+
+  padding: 1.5rem;
   background-color: ${theme.color.white}; 
-  padding: 2.5rem;
   box-shadow: ${theme.boxShadow.regular}; 
   margin-bottom: ${props => props.margin ? "3.75rem" : "0"};
   max-width: ${props => props.shorter ? "815px" : "100%"};
