0d9dbbd869de8331bf417b7fce0e4238f3202a1f
David Flindall <hello@backtofront.london>
2019-11-11 19:37:07 +0000

    styls for text split

--- src/components/text-split.js
+++ src/components/text-split.js
@@ -1,10 +1,15 @@
 import styled from "styled-components"
+import theme from "../theme"
 
 export default styled.div `
+
+  margin-bottom: ${theme.columnGap.regular};
   
   @media screen and (min-width: 576px) {
     display: flex;
     justify-content: space-between;
+    align-items: baseline;
+    margin-bottom: 0;
   }
 
 `
