1e96967719d60690834a5acf55733c08a185852b
David Flindall <hello@backtofront.london>
2019-11-11 17:02:52 +0000

    fix to icon loading in live environment

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,6 +14,7 @@ import TextWrapper from "../components/text-wrapper"
 import { createGlobalStyle } from "styled-components"
 import styled from "styled-components"
 import TextSplit from "../components/text-split"
+import "@fortawesome/fontawesome-svg-core/styles.css"
 
 const GlobalStyle = createGlobalStyle`
   body {
